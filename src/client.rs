39d7e216ae2ea54bce01ec11749d3a15438c62a3
Sam Vervaeck <samvv@pm.me>
2025-05-30 10:35:55 +0200

    Fix client interface not matching server

--- src/client.rs
+++ src/client.rs
@@ -6,7 +6,7 @@ use zbus::proxy;
     default_path = "/be/samvervaeck/InputDaemon"
 )]
 pub trait Service {
-    fn send_key(&mut self, code: u32, state: i32) -> zbus::Result<bool>;
-    fn send_wheel(&mut self, vertical: i32, horizontal: i32) -> zbus::Result<bool>;
-    fn send_motion(&mut self, x: i32, y: i32, z: i32) -> zbus::Result<bool>;
+    fn send_key(&mut self, code: u32, state: i32) -> zbus::Result<()>;
+    fn send_wheel(&mut self, vertical: i32, horizontal: i32) -> zbus::Result<()>;
+    fn send_motion(&mut self, x: i32, y: i32, z: i32) -> zbus::Result<()>;
 }
